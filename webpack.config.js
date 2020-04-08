b900a202f6d46802b7341074e9b2dc735071399c
SoterRamirez <soterrc@gmail.com>
2020-04-08 12:40:16 -0500

    Configuración final: ESLint y Git Ignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,52 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    },
-    resolve: {
-    extensions: [".js", ".jsx", "html"],
-    },
-    module: {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', 'html'],
+  },
+  module: {
     rules: [
-        {
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-            loader: "babel-loader",
+          loader: 'babel-loader',
         },
-        },
-        {
+      },
+      {
         test: /\.html$/,
         use: [
-            {
-            loader: "html-loader",
-            },
+          {
+            loader: 'html-loader',
+          },
         ],
-        },
-        {
-            test: /\.(s*)css$/,
-            use: [
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                },
-                'css-loader',
-                'sass-loader',
-            ],
-        },
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/[name].css'
-        }),
-    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
+    }),
+  ],
 };
