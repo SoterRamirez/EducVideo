d638b9f108114efa4cc44373b1a728e4062e6f97
SoterRamirez <soterrc@gmail.com>
2020-04-09 13:46:27 -0500

    Estructura y Estilos del Buscador

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Header from '../components/Header'
-import '../assets/styles/App.scss'
+import React from 'react';
+import Header from '../components/Header';
+import '../assets/styles/App.scss';
+import Search from '../components/Search';
 
 const App = () => (
     <div className="App">
     <Header />
+    <Search />
     </div>
 );
 
