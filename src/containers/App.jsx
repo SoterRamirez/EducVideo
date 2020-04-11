1b8a9b60a40d7ea994bf0edca5b5289f29b5b560
SoterRamirez <soterrc@gmail.com>
2020-04-10 20:28:06 -0500

    React Hooks: useEffect y useState

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -8,34 +8,45 @@ import Footer from '../components/Footer';
 
 import '../assets/styles/App.scss';
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () =>{
+    const[ videos, setVideos ] = useState ([]);
+    {/* Trear el api fake que tengo */}
+    useEffect(() => {
+        fetch('http://localhost:3000/initalState')
+            .then(response => response.json())
+            .then(data => setVideos(data));
+    }, []);
 
-        <Categories title="Mi lista">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Categories title="Originales de Educ Video">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-);
+    console.log(videos)
 
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+
+            <Categories title="Mi lista">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Categories title="Tendencias">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Categories title="Originales de Educ Video">
+                <Carousel>
+                    <CarouselItem />
+                    <CarouselItem />
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+        );
+}
 export default App;
