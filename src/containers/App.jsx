7da22ce812d9a891be7c0f288326a50098831334
SoterRamirez <soterrc@gmail.com>
2020-04-11 10:40:29 -0500

    Conectando la información de la API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,43 +5,41 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
-
 import '../assets/styles/App.scss';
 
 const App = () =>{
-    const[ videos, setVideos ] = useState ([]);
+    const [videos, setVideos] = useState({
+        mylist: [],
+        trends: [],
+        originals: [],
+      });
     {/* Trear el api fake que tengo */}
     useEffect(() => {
         fetch('http://localhost:3000/initalState')
             .then(response => response.json())
             .then(data => setVideos(data));
     }, []);
-
-    console.log(videos)
-
     return (
         <div className="App">
             <Header />
             <Search />
-
-            <Categories title="Mi lista">
-                <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
-                </Carousel>
-            </Categories>
+            {videos.mylist.length > 0 &&
+                <Categories title="Mi lista">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            }
             <Categories title="Tendencias">
                 <Carousel>
-                    <CarouselItem />
-                    <CarouselItem />
-                    <CarouselItem />
+                {
+                    videos.trends.map(item =>
+                        <CarouselItem key={item.id}{...item}/>
+                )}
                 </Carousel>
             </Categories>
             <Categories title="Originales de Educ Video">
                 <Carousel>
-                    <CarouselItem />
                     <CarouselItem />
                 </Carousel>
             </Categories>
