e13c5dc6131b26a8bfc07663d1e46d5ff80544e6
SoterRamirez <soterrc@gmail.com>
2020-04-15 08:38:24 -0500

    Finalizando reducers y eliminar favoritos

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,11 @@ const reducer =( state, action) => {
                 ...state,
                 myList: [...state.myList, action.payload]
             }
+        case 'DELETE_FAVORITE':
+            return{
+                ...state,
+                myList: state.myList.filter(items => items.id !== action.payload)
+            }
         default:
             return state;
     }
