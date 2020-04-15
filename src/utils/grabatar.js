52c49a7fab24a9d707ce55bda731b28964fcb25b
SoterRamirez <soterrc@gmail.com>
2020-04-15 12:13:06 -0500

    Uso de gravatar en nuestro proyecto

--- src/utils/grabatar.js
+++
@@ -1,8 +0,0 @@
-import md5 from 'md5';
-
-const grabatar = (email) => {
-    const base= 'https://grabatar.com/avatar/'
-    const formatteEmail = (email).trim().toLowerCase();
-    const hash = md5(formatteEmail, { encoding:"binary" });
-    return '${base}${hash}'
-}
